6bc2a5b0e5e3a9af89921908f92e43f74ddf61ad
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2014-10-22 00:06:56 +0100

    new delegate which uses std::bind ... (possibly not any better than the old way).

--- include/basics/delegate.h
+++ include/basics/delegate.h
@@ -1,7 +1,7 @@
 //
 //  delegate.h
 //
-//  Created by Edmund Kapusniak on 03/05/2014.
+//  Created by Edmund Kapusniak on 21/10/2014.
 //  Copyright (c) 2014 Edmund Kapusniak. All rights reserved.
 //
 
@@ -10,93 +10,168 @@
 #define DELEGATE_H
 
 
-class anyobject_t;
+/*
+    Returns a std::function which calls a method on an object.
+    
+    When the method has no overloads the signature of the method can be
+    deduced by the compiler:
+ 
+        delegate( object, &object::method )
+ 
+ 
+    Otherwise provide a function type to select a particular overload, e.g:
+ 
+        delegate< void( int ) >( object, &object::method )
+ 
+ 
+    I think this is all legal.  The part I am most wary of is the overloading
+    of the function template with the explicit-signature forms.
+*/
 
 
-template < typename return_t, typename ... arguments_t >
-class delegate_t
+
+#include <functional>
+
+
+namespace delegate_impl
 {
-public:
 
-    typedef return_t (*function_t)( arguments_t ... );
-    typedef return_t (anyobject_t::*method_t)( arguments_t ... );
-
-    delegate_t();
-    delegate_t( function_t function );
-    delegate_t( anyobject_t* object, method_t method );
-    
-    return_t operator () ( arguments_t ... arguments ) const;
-    
-private:
-
-    anyobject_t*    object;
-    union
-    {
-        method_t    method;
-        function_t  function;
-    };
-
+template < int n >
+struct placeholder
+{
 };
 
-
-
-template < typename return_t, typename ... arguments_t >
-inline delegate_t< return_t, arguments_t ... > delegate(
-                            return_t (*function)( arguments_t ... ) )
-{
-    typedef ::delegate_t< return_t, arguments_t ... > delegate_t;
-    return delegate_t( function );
 }
 
+namespace std
+{
 
+template < int n >
+struct is_placeholder< delegate_impl::placeholder< n > >
+    :   std::integral_constant< int, n >
+{
+};
+
+}
+
+namespace delegate_impl
+{
+
+template < typename object_t, typename signature_t >
+struct member_pointer;
 
 template < typename object_t, typename return_t, typename ... arguments_t >
-inline delegate_t< return_t, arguments_t ... > delegate(
-        object_t* object, return_t (object_t::*method)( arguments_t ... ) )
+struct member_pointer< object_t, return_t ( arguments_t ... ) >
 {
-    typedef ::delegate_t< return_t, arguments_t ... > delegate_t;
-    typedef typename delegate_t::method_t method_t;
-    return delegate_t( (anyobject_t*)object, (method_t)method );
+    typedef return_t (object_t::*type)( arguments_t ... );
+};
+
+template < typename object_t, typename signature_t >
+struct const_member_pointer;
+
+template < typename object_t, typename return_t, typename ... arguments_t >
+struct const_member_pointer< object_t, return_t ( arguments_t ... ) >
+{
+    typedef return_t (object_t::*type)( arguments_t ... ) const;
+};
+
+template < int n >
+struct bind_delegate
+{
+    template < typename object_t, typename return_t, typename ... arguments_t, typename ... placeholders_t >
+    static std::function< return_t( arguments_t ... ) > bind(
+                object_t* object, return_t (object_t::*method)( arguments_t ... ), placeholders_t ... placeholders )
+    {
+        return bind_delegate< n - 1 >::bind( object, method, placeholder< n >(), placeholders ... );
+    }
+
+    template < typename object_t, typename return_t, typename ... arguments_t, typename ... placeholders_t >
+    static std::function< return_t( arguments_t ... ) > bind(
+                const object_t* object, return_t (object_t::*method)( arguments_t ... ) const, placeholders_t ... placeholders )
+    {
+        return bind_delegate< n - 1 >::bind( object, method, placeholder< n >(), placeholders ... );
+    }
+};
+
+template <>
+struct bind_delegate< 0 >
+{
+    template < typename object_t, typename return_t, typename ... arguments_t, typename ... placeholders_t >
+    static std::function< return_t( arguments_t ... ) > bind(
+                object_t* object, return_t (object_t::*method)( arguments_t ... ), placeholders_t ... placeholders )
+    {
+        return std::bind( method, *object, placeholders ... );
+    }
+
+    template < typename object_t, typename return_t, typename ... arguments_t, typename ... placeholders_t >
+    static std::function< return_t( arguments_t ... ) > bind(
+                const object_t* object, return_t (object_t::*method)( arguments_t ... ) const, placeholders_t ... placeholders )
+    {
+        return std::bind( method, *object, placeholders ... );
+    }
+};
+    
+};
+
+template < typename object_t, typename return_t, typename ... arguments_t >
+std::function< return_t( arguments_t ... ) >
+            delegate( object_t* object, return_t (object_t::*method)( arguments_t ... ) )
+{
+    const int n = sizeof ... ( arguments_t );
+    return delegate_impl::bind_delegate< n >::bind( object, method );
 }
 
-
-
-template < typename return_t, typename ... arguments_t >
-inline delegate_t< return_t, arguments_t ... >::delegate_t()
-    :   object( nullptr )
-    ,   function( nullptr )
+template < typename object_t, typename return_t, typename ... arguments_t >
+std::function< return_t( arguments_t ... ) >
+            delegate( const object_t* object, return_t (object_t::*method)( arguments_t ... ) const )
 {
+    const int n = sizeof ... ( arguments_t );
+    return delegate_impl::bind_delegate< n >::bind( object, method );
 }
 
-
-template < typename return_t, typename ... arguments_t >
-inline delegate_t< return_t, arguments_t ... >::
-                    delegate_t( function_t function )
-    :   object( nullptr )
-    ,   function( function )
+template < typename object_t, typename return_t, typename ... arguments_t >
+std::function< return_t( arguments_t ... ) >
+            delegate( object_t& object, return_t (object_t::*method)( arguments_t ... ) )
 {
+    const int n = sizeof ... ( arguments_t );
+    return delegate_impl::bind_delegate< n >::bind( &object, method );
 }
 
-
-template < typename return_t, typename ... arguments_t >
-inline delegate_t< return_t, arguments_t ... >::
-                    delegate_t( anyobject_t* object, method_t method )
-    :   object( object )
-    ,   method( method )
+template < typename object_t, typename return_t, typename ... arguments_t >
+std::function< return_t( arguments_t ... ) >
+            delegate( const object_t& object, return_t (object_t::*method)( arguments_t ... ) const )
 {
+    const int n = sizeof ... ( arguments_t );
+    return delegate_impl::bind_delegate< n >::bind( &object, method );
 }
 
-
-template < typename return_t, typename ... arguments_t >
-inline return_t delegate_t< return_t, arguments_t ... >::
-                    operator () ( arguments_t ... arguments ) const
+template < typename signature_t, typename object_t >
+std::function< signature_t > delegate( object_t* object,
+            typename delegate_impl::member_pointer< object_t, signature_t >::type method )
 {
-    if ( object )
-        return ( object->*method )( arguments ... );
-    else
-        return function( arguments ... );
+    return delegate( object, method );
 }
 
+template < typename signature_t, typename object_t >
+std::function< signature_t > delegate( const object_t* object,
+            typename delegate_impl::const_member_pointer< object_t, signature_t >::type method )
+{
+    return delegate( object, method );
+}
+
+template < typename signature_t, typename object_t >
+std::function< signature_t > delegate( object_t& object,
+            typename delegate_impl::member_pointer< object_t, signature_t >::type method )
+{
+    return delegate( object, method );
+}
+
+template < typename signature_t, typename object_t >
+std::function< signature_t > delegate( const object_t& object,
+            typename delegate_impl::const_member_pointer< object_t, signature_t >::type method )
+{
+    return delegate( object, method );
+}
 
 
 
